3cefd0dcb1648f9e2470ce0ad6cd9ecea4c2db6c
Kartik <kiwi@Kartiks-MacBook-Pro-2.local>
2019-06-27 16:38:00 +0530

    feat: context Graph with Nodes & Edges information

--- src/contextGraph.js
+++ src/contextGraph.js
@@ -8,20 +8,45 @@ class ContextGraph extends React.Component {
 
   render(){
     const elements = [
-       { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
-       { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
-       { data: { id: 'three', label: 'Node 3' }, position: { x: 200, y: 50 } },
-       { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } },
-       { data: { source: 'two', target: 'three', label: 'Edge from Node12to Node3' } }
+       { data: { id: 'CCEM Reg', label: 'CCEM Reg1' }, position: { x: 0, y: 0 } },
+       { data: { id: 'CCEM Risk', label: 'CCEM Risk' }, position: { x: 100, y: 0 } },
+       { data: { id: 'PM', label: 'PM' }, position: { x: 0, y: 100 } },
+       { data: { id: 'DAC', label: 'DAC' }, position: { x: 100, y: 150 } },
+       { data: { source: 'CCEM Risk', target: 'DAC', label: 'FX Feed' } },
+       { data: { source: 'CCEM Reg', target: 'CCEM Risk', label: 'UxM Feed' } },
+       { data: { source: 'CCEM Reg', target: 'PM', label: 'Reg Feed' } },
+       { data: { source: 'CCEM Risk', target: 'PM', label: 'Risk, MTM, RADAR' } }
     ];
 
-    return <CytoscapeComponent 
-      
-      elements={elements} 
+
+    return <CytoscapeComponent
+      elements={elements}
       pan={ { x: 100, y: 200 } } 
-      style={ { width: '500px', height: '300px' } } 
+      style={ { width: '700px', height: '600px' } } 
       zoom={1.2} 
-    />;
+      stylesheet={[ // the stylesheet for the graph
+      {
+        selector: 'node',
+        style: {
+          'background-color': '#666',
+          'label': 'data(id)'
+        }
+      },
+
+      {
+        selector: 'edge',
+        style: {
+          'width': 3,
+          'length': 100,
+          'line-color': '#ccc',
+          'target-arrow-color': '#ccc',
+          'target-arrow-shape': 'diamond',
+          'label': 'data(label)'
+        }
+      }
+    ]}
+    />
+
   }
 }
 
